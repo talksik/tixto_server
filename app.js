48a57a4416a7e88d0adae00752e46040c804cae6
Arjun Patel <patel.arjun50@gmail.com>
2018-07-26 23:03:54 -0700

    fix to socket code

--- app.js
+++ app.js
@@ -87,13 +87,4 @@ io.on('connection', function (socket) {
   socket.on('disconnect', function() {
     console.log('Client disconnected: ' + socket.id);
   });
-
-  socket.on('error', function(err) {
-    console.log('db error', err);
-    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-      handleDisconnect();                         // lost due to either server restart, or a
-    } else {                                      // connnection idle timeout (the wait_timeout
-      throw err;                                  // server variable configures this)
-    }
-  });
 });
